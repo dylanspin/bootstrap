5dcf4232e34fd55678041984f572c0640bd6c7aa
dylan <dylanspin100@hotmail.com>
2019-06-28 18:47:38 +0200

    update

---
+++ jp1.js
@@ -0,0 +1,15 @@
+
+
+function link(t){
+  var id = t.id;
+  window.alert(id);
+
+  if(id == 1){window.location.href = "p1.html";}
+  if(id == 2){window.location.href ="p2.html";}
+  if(id == 3){window.location.href ="p3.html";}
+  if(id == 4){window.location.href ="p4.html";}
+  if(id == 5){window.location.href ="p5.html";}
+  if(id == 6){window.location.href ="p6.html";}
+  if(id == 7){window.location.href ="p7.html";}
+  if(id == 8){window.location.href ="p8.html";}
+}
