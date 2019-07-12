e6686ea9d0274c4e90089a0a6204c042cb457b02
dylan <dylanspin100@hotmail.com>
2019-07-12 22:20:32 +0200

    voor laptop pull

--- jp1.js
+++ jp1.js
@@ -4,17 +4,8 @@ function link(t){
   var id = t.id;
   window.alert(id);
 
-  if(id == 1){window.location.href = "p1.html";}
-  if(id == 2){window.location.href ="p2.html";}
-  if(id == 3){window.location.href ="p3.html";}
-  if(id == 4){window.location.href ="p4.html";}
-  if(id == 5){window.location.href ="p5.html";}
-  if(id == 6){window.location.href ="p6.html";}
-  if(id == 7){window.location.href ="p7.html";}
-  if(id == 8){window.location.href ="p8.html";}
+  if(id == 1){window.location.href = "operators.html";}
   if(id == 9){window.location.href ="news.html";}
-  if(id == 10){window.location.href ="p8.html";}
-  if(id == 11){window.location.href ="p8.html";}
 }
 function inlog(){
   var naam = document.getElementById('voornaam').value;
